ac795b5628311110263fefc6c1346d11c0153b38
MiaJup75 <miajupiter75@gmail.com>
2025-05-29 23:20:51 +0700

    Update new_tokens.py

--- new_tokens.py
+++ new_tokens.py
@@ -1,3 +1,5 @@
+# new_tokens.py – New Token Detector
+
 def get_new_tokens():
     # TODO: Implement real logic to fetch new tokens
-    return "🆕 <b>No new tokens detected (stub).</b>"
+    return "🆕 <b>No new tokens detected at this time.</b>"
